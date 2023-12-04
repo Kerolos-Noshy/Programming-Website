d4157a40ea4dcc36ed965d3a2562c31d220a75ad
Kerolos Noshy <101178275+Kerolos-Noshy@users.noreply.github.com>
2023-12-04 17:39:33 +0200

    add last version

--- js/script.js
+++ js/script.js
@@ -1,28 +1,20 @@
 function openPage(htmlFileName) {
-  window.location.href = "/pages/" + htmlFileName + ".html";
+  window.location.href = "pages/" + htmlFileName + ".html";
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   var currentActiveElement = document.querySelector(".sidebar-item.active");
 
-  // Function to toggle active class
   function toggleActive(clickedElement) {
-    // Remove 'active' class from the previously active element
     if (currentActiveElement !== null) {
       currentActiveElement.classList.remove("active");
-      // change the margin of active element by subtranging 5 from it
-      currentActiveElement.style.paddingLeft = "30px";
     }
 
-    // Add 'active' class to the clicked element
     clickedElement.classList.add("active");
-    clickedElement.style.paddingLeft = "25px";
 
-    // Update the currently active element
     currentActiveElement = clickedElement;
   }
 
-  // Add click event listeners to all sidebar items
   var sidebarItems = document.querySelectorAll(".sidebar-item");
   sidebarItems.forEach(function (item) {
     item.addEventListener("click", function () {
@@ -35,20 +27,17 @@ document.addEventListener("DOMContentLoaded", function () {
   var sidebarItems = document.querySelectorAll(".sidebar-item");
 
   window.addEventListener("scroll", function () {
-    var fromTop = window.scrollY + 105; // Adjusted to 100px from the top
+    var fromTop = window.scrollY + 105; // Adjusted to 105px from the top
 
     var container = document.querySelector(".container");
     var containerTop = container.offsetTop;
     var containerHeight = container.offsetHeight;
 
-    // Check if scroll position is within the container
     if (fromTop > containerTop && fromTop < containerTop + containerHeight) {
-      // Remove 'active' class from all items
       sidebarItems.forEach(function (item) {
         item.classList.remove("active");
       });
     } else {
-      // Check for section visibility and update 'active' class accordingly
       sidebarItems.forEach(function (item) {
         var section = document.getElementById(
           item.getAttribute("href").substring(1)
@@ -58,15 +47,86 @@ document.addEventListener("DOMContentLoaded", function () {
           section.offsetTop <= fromTop &&
           section.offsetTop + section.offsetHeight > fromTop
         ) {
-          // Remove 'active' class from all items
           sidebarItems.forEach(function (item) {
             item.classList.remove("active");
           });
 
-          // Add 'active' class to the current item
           item.classList.add("active");
         }
       });
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const loginDiv = document.querySelector(".login");
+  const signupDiv = document.querySelector(".signup");
+
+  const loginLink = document.getElementById("login-link");
+  const signupLink = document.getElementById("signup-link");
+
+  loginLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    loginDiv.style.display = "flex";
+    signupDiv.style.display = "none";
+  });
+
+  signupLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    loginDiv.style.display = "none";
+    signupDiv.style.display = "flex";
+  });
+});
+
+function scrollToTop() {
+  var container = document.querySelector(".container");
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+window.onscroll = function () {
+  toggleScrollToTopButton();
+};
+
+function toggleScrollToTopButton() {
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  var containerHeight = document.querySelector(".container").offsetHeight;
+
+  if (
+    document.body.scrollTop > containerHeight - 150 ||
+    document.documentElement.scrollTop > containerHeight - 150
+  ) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+function showMenu(level) {
+  hideAllLevels();
+
+  const levelElement = document.querySelector(`.${level}`);
+  levelElement.style.display = "flex";
+}
+
+function hideMenu(level) {
+  // hideAllLevels();
+  const levelElement = document.querySelector(`.${level}`);
+  levelElement.style.display = "none";
+}
+
+function hideAllLevels() {
+  document.querySelector(".web").style.display = "none";
+  document.querySelector(".soft").style.display = "none";
+}
+
+function activeParent(level) {
+  const levelElement = document.querySelector(`.${level}`);
+  levelElement.style.backgroundColor = "rgba(5, 86, 243, 0.2)";
+}
+
+function deactiveParent(level) {
+  const levelElement = document.querySelector(`.${level}`);
+  levelElement.style.backgroundColor = "transparent";
+}
